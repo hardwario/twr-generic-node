c278861df87e60edad3012abcb6fbe7aa2f8b2fa
Karel Blavka <karel.blavka@bigclown.com>
2017-06-10 12:38:11 +0200

    Add radio.h

---
+++ app/radio.h
@@ -0,0 +1,24 @@
+#ifndef _APP_RADIO_H
+#define _APP_RADIO_H
+
+#define RADIO_LED	 			0x00
+#define RADIO_LED_SET 			0x01
+#define RADIO_LED_GET 			0x02
+
+#define RADIO_RELAY_0	 			0x03
+#define RADIO_RELAY_0_SET 			0x04
+#define RADIO_RELAY_0_GET 			0x05
+
+#define RADIO_RELAY_1	 			0x06
+#define RADIO_RELAY_1_SET 			0x07
+#define RADIO_RELAY_1_GET 			0x08
+
+#define RADIO_RELAY_POWER 			0x09
+#define RADIO_RELAY_POWER_SET 		0x0a
+#define RADIO_RELAY_POWER_GET 		0x0b
+
+#define RADIO_LED_STRIP_COLOR_SET 		0x10
+#define RADIO_LED_STRIP_BRIGHTNESS_SET 	0x11
+#define RADIO_LED_STRIP_COMPOUND_SET 	0x12
+
+#endif /* APP_RADIO_H_ */
