02b8e4889b53c604397872e91f106914ddf0c58d
Karel Blavka <karel.blavka@bigclown.com>
2017-08-05 16:16:50 +0200

    Add menu for led-strip effects

--- app/application.c
+++ app/application.c
@@ -18,8 +18,10 @@
 #define BAROMETER_TAG_UPDATE_INTERVAL (1 * 1000)
 
 #if MODULE_POWER
+#define MAX_PAGE_INDEX 3
 #define CO2_UPDATE_INTERVAL (15 * 1000)
 #else
+#define MAX_PAGE_INDEX 2
 #define CO2_UPDATE_INTERVAL (1 * 60 * 1000)
 #endif
 
@@ -58,9 +60,18 @@ static const struct
 };
 
 static int page_index = 0;
+static int menu_item = 0;
 
 #if MODULE_POWER
 static uint64_t my_device_address;
+
+static char *menu_items[] = {
+        "Page 0",
+        "LED Test",
+        "Effect Rainbow",
+        "Effect Rainbow cycle",
+        "Effect Theater chase rainbow"
+};
 
 static uint32_t _bc_module_power_led_strip_dma_buffer[LED_STRIP_COUNT * LED_STRIP_TYPE * 2];
 const bc_led_strip_buffer_t led_strip_buffer =
@@ -259,27 +270,45 @@ void application_task(void)
 
     bc_module_lcd_clear();
 
-    bc_module_lcd_set_font(&bc_font_ubuntu_15);
-    bc_module_lcd_draw_string(10, 5, pages[page_index].name0, true);
+    if (page_index < 3)
+    {
+        bc_module_lcd_set_font(&bc_font_ubuntu_15);
+        bc_module_lcd_draw_string(10, 5, pages[page_index].name0, true);
 
-    bc_module_lcd_set_font(&bc_font_ubuntu_28);
-    snprintf(str, sizeof(str), pages[page_index].format0, *pages[page_index].value0);
-    w = bc_module_lcd_draw_string(25, 25, str, true);
-    bc_module_lcd_set_font(&bc_font_ubuntu_15);
-    w = bc_module_lcd_draw_string(w, 35, pages[page_index].unit0, true);
+        bc_module_lcd_set_font(&bc_font_ubuntu_28);
+        snprintf(str, sizeof(str), pages[page_index].format0, *pages[page_index].value0);
+        w = bc_module_lcd_draw_string(25, 25, str, true);
+        bc_module_lcd_set_font(&bc_font_ubuntu_15);
+        w = bc_module_lcd_draw_string(w, 35, pages[page_index].unit0, true);
 
-    bc_module_lcd_set_font(&bc_font_ubuntu_15);
-    bc_module_lcd_draw_string(10, 55, pages[page_index].name1, true);
+        bc_module_lcd_set_font(&bc_font_ubuntu_15);
+        bc_module_lcd_draw_string(10, 55, pages[page_index].name1, true);
 
-    bc_module_lcd_set_font(&bc_font_ubuntu_28);
-    snprintf(str, sizeof(str), pages[page_index].format1, *pages[page_index].value1);
-    w = bc_module_lcd_draw_string(25, 75, str, true);
-    bc_module_lcd_set_font(&bc_font_ubuntu_15);
-    bc_module_lcd_draw_string(w, 85, pages[page_index].unit1, true);
+        bc_module_lcd_set_font(&bc_font_ubuntu_28);
+        snprintf(str, sizeof(str), pages[page_index].format1, *pages[page_index].value1);
+        w = bc_module_lcd_draw_string(25, 75, str, true);
+        bc_module_lcd_set_font(&bc_font_ubuntu_15);
+        bc_module_lcd_draw_string(w, 85, pages[page_index].unit1, true);
+    }
+#if MODULE_POWER
+    else
+    {
+        bc_module_lcd_set_font(&bc_font_ubuntu_13);
 
-    snprintf(str, sizeof(str), "%d/3", page_index + 1);
+        for (int i = 0; i < 5; i++)
+        {
+            bc_module_lcd_draw_string(5, 5 + (i * 15), menu_items[i], i != menu_item);
+        }
 
-    bc_module_lcd_draw_string(100, 108, str, true);
+        bc_module_lcd_set_font(&bc_font_ubuntu_13);
+        bc_module_lcd_draw_string(5, 115, "Down", true);
+        bc_module_lcd_draw_string(90, 115, "Enter", true);
+    }
+#endif
+
+    snprintf(str, sizeof(str), "%d/%d", page_index + 1, MAX_PAGE_INDEX + 1);
+    bc_module_lcd_set_font(&bc_font_ubuntu_13);
+    bc_module_lcd_draw_string(55, 115, str, true);
 
     bc_module_lcd_update();
 
@@ -393,21 +422,68 @@ void lcd_button_event_handler(bc_button_t *self, bc_button_event_t event, void *
 
     if (self->_channel.virtual_channel == BC_MODULE_LCD_BUTTON_LEFT)
     {
-        page_index--;
-        if (page_index < 0)
+        if ((page_index != 3))
         {
-            page_index = 2;
+            // Key prew page
+            page_index--;
+            if (page_index < 0)
+            {
+                page_index = MAX_PAGE_INDEX;
+                menu_item = 0;
+            }
         }
+        else
+        {
+            // Key menu down
+            menu_item++;
+            if (menu_item == 5)
+            {
+                menu_item = 0;
+            }
+        }
+
         static uint16_t left_event_count = 0;
         _radio_pub_u16(RADIO_LCD_BUTTON_LEFT, left_event_count++);
     }
     else
     {
-        page_index++;
-        if (page_index > 2)
+        if ((page_index != 3) || (menu_item == 0))
         {
-            page_index = 0;
+            // Key next page
+            page_index++;
+            if (page_index > MAX_PAGE_INDEX)
+            {
+                page_index = 0;
+            }
+            if (page_index == 3)
+            {
+                menu_item = 0;
+            }
         }
+#if MODULE_POWER
+        else if (page_index == 3)
+        {
+            // Key enter
+            if (menu_item == 1)
+            {
+                bc_led_strip_effect_test(&led_strip.self);
+            }
+            else if (menu_item == 2)
+            {
+                bc_led_strip_effect_rainbow(&led_strip.self, 50);
+            }
+            else if (menu_item == 3)
+            {
+                bc_led_strip_effect_rainbow_cycle(&led_strip.self, 50);
+            }
+            else if (menu_item == 4)
+            {
+                bc_led_strip_effect_theater_chase_rainbow(&led_strip.self, 50);
+            }
+            led_strip.show = LED_STRIP_SHOW_EFFECT;
+        }
+#endif
+
         static uint16_t right_event_count = 0;
         _radio_pub_u16(RADIO_LCD_BUTTON_RIGHT, right_event_count++);
     }
